c4771e27667fc1e14e0cdf789efacfe933d32dee
Brayan Rodríguez <brayanrodbajo@gmail.com>
2018-10-24 10:06:26 -0500

    tiempo de espera promedio works with filters

--- database.py
+++ database.py
@@ -21,7 +21,7 @@ def config(filename='database.ini', section='postgresql'):
     return db
 
 
-def query_tiempo_espera_horas(filter='ips'):
+def query_tiempo_espera_horas():
     """ Connect to the PostgreSQL database server """
     conn = None
     try:
@@ -35,14 +35,51 @@ def query_tiempo_espera_horas(filter='ips'):
         # create a cursor
         cur = conn.cursor()
 
-        if filter == 'ips':
-            # execute a statement
-            cur.execute('SELECT dim_ips.municipio, dim_ips.nombre, AVG(tiempo_espera_horas)::numeric::integer as horas_de_espera'
-            +' FROM citas_medicas, dim_ips'
-            +' WHERE dim_ips.key_ips = citas_medicas. key_ips'
-            +' GROUP BY dim_ips.nombre, dim_ips.municipio'
-            +' ORDER BY avg(tiempo_espera_horas) DESC'
-            )
+        # execute a statement
+        cur.execute('SELECT dim_ips.municipio, dim_ips.nombre, AVG(tiempo_espera_horas)::numeric::integer as horas_de_espera'
+                    +' FROM citas_medicas, dim_ips'
+                    +' WHERE dim_ips.key_ips = citas_medicas. key_ips'
+                    +' GROUP BY dim_ips.nombre, dim_ips.municipio'
+                    +' ORDER BY avg(tiempo_espera_horas) DESC'
+                    )
+
+        print("The number of rows: ", cur.rowcount)
+        # display the rows
+        rows = cur.fetchall()
+        # print(rows[:10])
+        return rows
+
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+def query_numero_pacientes():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute a statement
+        cur.execute('SELECT dim_ips.municipio, dim_ips.nombre, AVG(tiempo_espera_horas)::numeric::integer as horas_de_espera'
+                    +' FROM citas_medicas, dim_ips'
+                    +' WHERE dim_ips.key_ips = citas_medicas. key_ips'
+                    +' GROUP BY dim_ips.nombre, dim_ips.municipio'
+                    +' ORDER BY avg(tiempo_espera_horas) DESC'
+                    )
 
         print("The number of rows: ", cur.rowcount)
         # display the rows
---
+++ static/js/tiempo_espera.js
@@ -0,0 +1,8 @@
+
+var table_Props = {
+    col_1: "select",
+    col_0: "none",
+    display_all_text: "Todos",
+    sort_select: true
+};
+var tf2 = setFilterGrid("table", table_Props);
